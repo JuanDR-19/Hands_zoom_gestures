6f17ce748cb9eaaac112ec3de404f43c40df23dc
JuanDR-19 <juanmanuelduranrueda@gmail.com>
2022-11-01 08:18:00 -0500

    .

--- Image_zoom_gesture.py
+++ Image_zoom_gesture.py
@@ -1,4 +1,5 @@
 from cvzone.HandTrackingModule import HandDetector as hd
+from cvzone.FaceDetectionModule import FaceDetector as fd
 import mediapipe as mp
 import cv2
 
@@ -13,12 +14,12 @@ while True:
     hands, img = detector.findHands(img)
     img2 = cv2.imread("Cat03.jpg")
 
-    # img[0:1026, 0:1024] = img2
+    # img[0:1280, 0:1000] = img2
 
     if len(hands) == 2:
         print("tiene 2 manos en camara")
-        if detector.fingersUp(hands[0][0]) and detector.fingersUp(hands[0][1]) and detector.fingersUp(hands[1][0]) and detector.fingersUp(hands[1][1]):
-            print("gesto de zoom")
+        # if detector.fingersUp(hands[0][0]) and detector.fingersUp(hands[0][1]) and detector.fingersUp(hands[1][0]) and detector.fingersUp(hands[1][1]):
+            # print("gesto de zoom")
 
 
     cv2.imshow("camara", img)
